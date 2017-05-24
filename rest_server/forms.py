33316ee4b94e28d0d09c4c1fd86fedfc73983369
eliseev-eag <eliseev-eag@yandex.ru>
2017-05-24 18:30:01 +0700

    Поправил фильтры для поиска: - Реализовал подгрузку типов событий из БД - Поля дат начала и дат конца сделал опциональными, - Задал ширину select полей 100%

--- rest_server/forms.py
+++ rest_server/forms.py
@@ -1,31 +1,33 @@
 from django import forms
-from django.forms import CheckboxSelectMultiple, SelectMultiple, Select
+from django.forms import SelectMultiple
 
 from rest_server.models import Event
 
 
 class EventSearchForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(EventSearchForm, self).__init__(*args, **kwargs)
-        for field in iter(self.fields):
-            self.fields[field].widget.attrs.update({
-                'class': 'form-control'
-            })
+    event_type = forms.MultipleChoiceField(choices=[], required=False, label='Тип события',
+                                           widget=SelectMultiple(attrs={'size': 2}))
+
+    start_date = forms.DateField(required=False, label='Дата начала')
+    end_date = forms.DateField(required=False, label='Дата окончания')
 
     class Meta:
         model = Event
         fields = ('start_date', 'end_date', 'event_type', 'toponyms', 'persons', 'name')
         labels = {
             'name': '',
-            'start_date': 'Дата начала',
-            'end_date': 'Дата окончания',
-            'event_type': 'Тип события',
             'toponyms': 'Встречаемые топонимы',
             'persons': 'Действующие лица'
         }
         widgets = {
-            'event_type': SelectMultiple(choices=(('sport', 'Спортивное событие',), ('war', 'Военное событие')),
-                                         attrs={'size': 2}),
             'toponyms': SelectMultiple(attrs={'size': 2}),
             'persons': SelectMultiple(attrs={'size': 2}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(EventSearchForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+        self.fields['event_type'].choices = Event.objects.values_list('event_type', 'event_type').distinct()
